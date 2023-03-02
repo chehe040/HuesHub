feefacef0163e55dd820ae7b56f5d883ac6859b1
Uday Mehtani <udaymehtani@gmail.com>
2023-03-02 23:09:31 +0530

    Minor styling changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
-
 let color1 = document.getElementById("FIRST");
 let color2 = document.getElementById("SECOND");
 let gradientCont = document.getElementById("output");
 let btn = document.getElementById("btnThree");
 let copyButton = document.getElementById("btnCopy");
-let popUp = document.querySelector('.popup');
+let popUp = document.querySelector(".popup");
 // const hamburger = document.querySelector(".hamburger");
 // const navMenu = document.querySelector(".nav-menu");
 //
@@ -15,16 +14,13 @@ let popUp = document.querySelector('.popup');
 //     navMenu.classList.toggle("active");
 // }
 
-
-
-
-function makeColor() {
+const makeColor = () => {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
   return randomColor;
-}
+};
 
-function generateGradient() {
+const generateGradient = () => {
   color1.value = "#" + makeColor();
   color2.value = "#" + makeColor();
 
@@ -33,37 +29,39 @@ function generateGradient() {
   document.getElementById("codeOne").innerText = color1.value;
   document.getElementById("codeTwo").innerText = color2.value;
   gradientCont.style.background = `linear-gradient(${color1.value}, ${color2.value})`;
-  document.getElementById("outputCode").innerText = `background: linear-gradient(${color1.value}, ${color2.value});`;
-}
+  document.getElementById(
+    "outputCode"
+  ).innerText = `background: linear-gradient(${color1.value}, ${color2.value});`;
+};
 
-function setGradient() {
+const setGradient = () => {
   gradientCont.style.background = `linear-gradient(${color1.value}, ${color2.value})`;
   document.getElementById("codeOne").innerText = color1.value;
   document.getElementById("codeTwo").innerText = color2.value;
-  document.getElementById("outputCode").innerText = `background: linear-gradient(${color1.value}, ${color2.value});`;
-}
-
+  document.getElementById(
+    "outputCode"
+  ).innerText = `background: linear-gradient(${color1.value}, ${color2.value});`;
+};
 
 document.body.addEventListener("load", generateGradient());
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 btn.addEventListener("click", generateGradient);
 
-
-copyButton.addEventListener("click", ()=>{
-  popUp.classList.add('active');
+copyButton.addEventListener("click", () => {
+  popUp.classList.add("active");
   copyToClip();
 });
-popUp.addEventListener("animationend", ()=>{
-  popUp.classList.remove('active');
+popUp.addEventListener("animationend", () => {
+  popUp.classList.remove("active");
 });
-function copyToClip(){
-  const textarea = document.createElement('textarea');
-  textarea.setAttribute('readonly', '');
-  textarea.style.position = 'absolute';
+const copyToClip = () => {
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("readonly", "");
+  textarea.style.position = "absolute";
   textarea.value = document.getElementById("outputCode").innerText;
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(textarea);
-}
+};
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Hues Hub
+
+The Random Gradient Generator is a website that generates a random gradient using two colors. The website allows you to easily generate a new gradient by clicking on the "Random" button, which will display two colors in seperate containers, along with their corresponding hex codes. The CSS code for the gradient is also displayed, with a copy button to paste into your own projects.
+
+## How to Use
+
+To generate a random gradient, simply click on the "Random" button. You can also choose your own colors by clicking on the color picker in each container. Once you have chosen your colors, the CSS code for the gradient will be displayed, and you can copy and paste it into your own project.
+
+## Features
+
+- Randomly generates a gradient using two colors
+- Displays hex codes for each color
+- Displays CSS code for the generated gradient
+- Allows you to choose your own colors using the color picker
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Author
+
+This website was created by [@chehe040](https://github.com/chehe040)
+
+
+## Contact
+
+If you have any questions or suggestions, please feel free to contact me at chetnass040@gmail.com.
